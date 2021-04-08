8417f6aae3e7c36e15d7c72f6693aad0d9a6e664
sanketh bhongir <sankethbhongir06@gmail.com>
2021-04-07 23:38:31 -0400

    added a test case

--- square_root_problem/sqrt.py
+++ square_root_problem/sqrt.py
@@ -34,3 +34,5 @@ print ("Pass" if  (0 == sqrt(0)) else "Fail")
 print ("Pass" if  (4 == sqrt(16)) else "Fail")
 print ("Pass" if  (1 == sqrt(1)) else "Fail")
 print ("Pass" if  (5 == sqrt(27)) else "Fail")
+print ("Pass" if  (5 == sqrt(196)) else "Fail")
+
